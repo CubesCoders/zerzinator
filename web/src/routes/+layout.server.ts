9e3bbabc24037c4c048425c9f6fcb85d664314c8
CubeCoder <markus.hamacher16@gmail.com>
2024-04-14 14:05:01 +0200

    Add URL logging in load function of +layout.server.ts

--- web/src/routes/+layout.server.ts
+++ web/src/routes/+layout.server.ts
@@ -2,7 +2,8 @@ import { superValidate } from "sveltekit-superforms";
 import { loginSchema, registerSchema } from "./schema.js";
 import { zod } from "sveltekit-superforms/adapters";
 
-export async function load({ locals }) {
+export async function load({ locals, url }) {
+	console.log(url.origin);
 	if (locals.user) {
 		return {
 			user: locals.user,
